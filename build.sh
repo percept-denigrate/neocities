4eebef4d6648fcc54101a20f038988f778c3b8d5
percept-denigrate <evanpontonnier@gmail.com>
2025-11-15 10:41:21 +0100

    autobuild

---
+++ build.sh
@@ -0,0 +1,13 @@
+INPUT_DIR=source
+OUTPUT_DIR=public
+
+rm "$OUTPUT_DIR"/*.html
+
+for file in "$INPUT_DIR"/*.md; do
+    filename=$(basename "$file" .md)
+    pandoc "$file" \
+        --template=template.html \
+	--metadata title="website" \
+        -o "$OUTPUT_DIR/$filename.html"
+done
+
---
+++ source/test.md
@@ -0,0 +1,25 @@
+# Title
+
+intro
+
+## Part 1: the beginning
+
+## 1.1
+
+**foo**
+
+[index](index)
+
+## 1.2
+
+blabla 
+
+> foo
+> 
+> bar
+
+## Part 2: how it went wrong
+
+*blabla*
+
+![caption](cat.jpg)
